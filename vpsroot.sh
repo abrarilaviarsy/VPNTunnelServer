7c6ce4c63a7ab664b0d79ca0e622b7ab5ec109ee
abrarilaviarsy <83096795+abrarilaviarsy@users.noreply.github.com>
2025-07-01 18:32:08 +0700

    Update vpsroot.sh

--- vpsroot.sh
+++ vpsroot.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 # Mod By SL
-#echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/sshd_config;
-systemctl restart sshd;
-clear;
-echo -e "Masukkan Password:";
-read -e pwe;
-usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
-clear;
-printf "Mohon Simpan Informasi Akun VPS Ini
+
+# Ganti konfigurasi SSH agar root bisa login pakai key saja
+wget -qO - -O /etc/ssh/sshd_config https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/sshd_config
+systemctl restart sshd
+clear
+
+# Minta user isi password (opsional, kamu bisa disable bagian ini kalau gak mau password sama sekali)
+echo -e "Masukkan Password:"
+read -e pwe
+
+# Set password root secara manual (optional — bisa dihapus kalau kamu pakai key-only login)
+usermod -p "$(perl -e "print crypt('$pwe','Q4')")" root
+
+clear
+
+# Tampilkan informasi login
+cat <<EOF
+
+Mohon Simpan Informasi Akun VPS Ini
 ============================================
 Akun Root (Akun Utama)
 Ip address = $(curl -Ls http://ipinfo.io/ip)
 Username   = root
 Password   = $pwe
 ============================================
-echo "";
-exit;
+
+EOF
+
+exit
